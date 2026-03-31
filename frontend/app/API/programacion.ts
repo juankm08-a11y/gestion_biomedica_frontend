911b79a8cbee968b3becc7f11efd0be24f337451
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2026-03-31 15:32:50 -0500

    Implementnado post y put en Programacion Mantenimiento

---
+++ frontend/app/API/programacion.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const createProgramacion = async () => {
+  const response = await axios.post(
+    "/mantenimientos/programacion_mantenimiento/",
+  );
+
+  return response.data;
+};
+
+export const updateProgramacion = async () => {
+  const response = await axios.put(
+    "/mantenimientos/programacion_mantenmiento/<int:pk>/",
+  );
+  return response.data;
+};
