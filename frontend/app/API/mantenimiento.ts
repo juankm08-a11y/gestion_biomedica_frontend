8bd82c6ee8dcf53cd39f0795d45840cc04cf711a
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2026-03-31 16:27:27 -0500

    change names in mantenimiento.ts and add return faltante

--- frontend/app/API/mantenimiento.ts
+++ frontend/app/API/mantenimiento.ts
@@ -1,15 +1,16 @@
 import axios from "axios";
 
-export const createMantemimiento = async () => {
+export const registrarMantenimiento = async () => {
   const response = await axios.post("/mantenimientos/");
   return response.data;
 };
 
-export const updateMantenimiento = async (id: number) => {
+export const actualizarMantenimiento = async (id: number) => {
   const response = await axios.put("/mantenimientos/<int:pk>/");
   return response.data;
 };
 
-export const deleteMantenimiento = async (id: number) => {
+export const eliminarMantenimiento = async (id: number) => {
   const response = await axios.delete("/mantenmientos/<int:pk>/");
+  return response.data;
 };
