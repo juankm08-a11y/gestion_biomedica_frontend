b19a63a9086bccbc5eaeda435604e6a9ca716410
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2026-03-31 17:51:22 -0500

    agrupando rutas por modulo

--- frontend/app/routes/routes.ts
+++ frontend/app/routes/routes.ts
@@ -1,10 +1,21 @@
 export const ROUTES = {
-  EQUIPOS: "/equipos",
-  EQUIPO_CREAR: "/equipos/registro_equipos",
-  EQUIPO_DETALLE: (id: number) => `/equipos/${id}`,
-  MANTENIMIENTO_CREAR: "/mantenimientos/registro_mantenimientos/",
-  CODIGOQR_GENERAR: "/codigosQr/",
-  REPORTE_REGISTRAR: "/reportes/registro_reportes/",
-  USUARIO_REGISTRAR: "/usuarios/registro_usuario/",
-  USUARIO_AUTENTICAR: "/usuarios/login_usuario/",
+  equipos: {
+    EQUIPOS: "/equipos",
+    EQUIPO_CREAR: "/equipos/registro",
+    EQUIPO_DETALLE: (id: number) => `/equipos/${id}`,
+  },
+
+  mantenimientos: {
+    MANTENIMIENTO_CREAR: "/mantenimientos/registro_mantenimientos",
+  },
+  codigosQr: {
+    CODIGOQR_GENERAR: "/codigosQr/",
+  },
+  reportes: {
+    REPORTE_REGISTRAR: "/reportes/registro_reportes",
+  },
+  usuarios: {
+    USUARIO_REGISTRAR: "/usuarios/registro_usuario",
+    USUARIO_AUTENTICAR: "/usuarios/login_usuario",
+  },
 };
