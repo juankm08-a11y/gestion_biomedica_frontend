02eeb460c1d1b9760207c4ef9e0cb6e0a617bad7
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2026-03-31 16:25:43 -0500

    change names in function in equipo.ts

--- frontend/app/API/equipo.ts
+++ frontend/app/API/equipo.ts
@@ -1,26 +1,26 @@
 import { api } from "./api";
 
-export const createEquipo = async () => {
+export const registrarEquipo = async () => {
   const response = await api.post("/equipos/");
   return response.data;
 };
 
-export const getEquipos = async () => {
+export const consultarEquipo = async () => {
   const response = await api.get("/equipos/");
   return response.data;
 };
 
-export const getEquipo = async (id: number) => {
+export const consultarEquipos = async (id: number) => {
   const response = await api.get("/equipos/${id}");
   return response.data;
 };
 
-export const putEquipo = async (id: number) => {
+export const actualizarEquipo = async (id: number) => {
   const response = await api.put("/equipos/${id}/");
   return response.data;
 };
 
-export const deleteEquipo = async (id: number) => {
+export const eliminarEquipo = async (id: number) => {
   const response = await api.delete("/equipos/${id}");
   return response.data;
 };
